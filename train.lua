a5b19f393f23986f79fa1c61c192dd2545441a2d
Shijie Wu <wushijie5@gmail.com>
2016-05-30 01:30:23 +0800

    bug fix and complete eval with confusion matrix, improve train process

--- train.lua
+++ train.lua
@@ -1,7 +1,8 @@
-MODEL_PREFIX = 'p82'
+
+MODEL_PREFIX = 'm3'
 MODEL_SUFFIX = 's1'
 PREFIX = MODEL_PREFIX .. '-' .. MODEL_SUFFIX
-USE_BN = true
+USE_BN = false
 NUM_OF_FEAT = 23
 NUM_OF_DAY = 31
 NUM_OF_CLASS = 2 -- AT = 1, LT = 2
@@ -10,9 +11,9 @@ CLASS_LT = 2.0
 BAT_SIZE = 10
 ATLT_NORM = {0, 0.1, 0.5} -- mean,std,shift
 HID_SIZE = 64
-RHO = 10
-BAT_SIZE = 10
-DROPOUT = 0.25
+RHO = 999999 -- 30
+BAT_SIZE = 256
+DROPOUT = 0 -- 0.5 disable dropout when 0
 LEARNING_RATE = 0.01
 OPTIM_METHOD = 'adam'
 OPTIM_PARA = {}
@@ -28,14 +29,15 @@ nn = require 'nn'
 rnn = require 'rnn'
 math = require 'math'
 dataload = require 'dataload'
-U = require 'util'
+util = require 'util'
 require 'nngraph'
+require 'cunn'
 
 torch.manualSeed(torch.initialSeed())
 
 -- 1. load train data
-tr_lt = U.load_data('tr_1m_LT_23pr')
-tr_at = U.load_data('tr_1m_AT_23pr')
+tr_lt = util.load_data('tr_1m_LT_23pr')
+tr_at = util.load_data('tr_1m_AT_23pr')
 print(#tr_lt, #tr_at)
 NUM_OF_TR_LT = (#tr_lt)[1]
 NUM_OF_TR_AT = (#tr_at)[1]
@@ -46,11 +48,16 @@ at_dataloader = dataload.TensorLoader(tr_at, tr_at_target)
 lt_dataloader = dataload.TensorLoader(tr_lt, tr_lt_target)
 
 -- 2. load val data
--- val_lt = U.load_data('val_1m_LT_23pr')
--- val_at = U.load_data('val_1m_AT_23pr')
--- print(#val_lt, #val_at)
--- NUM_OF_VAL_LT = (#val_lt)[1]
--- NUM_OF_VAL_AT = (#val_at)[1]
+val_lt = util.load_data('val_1m_LT_23pr')
+val_at = util.load_data('val_1m_AT_23pr')
+print(#val_lt, #val_at)
+NUM_OF_VAL_LT = (#val_lt)[1]
+NUM_OF_VAL_AT = (#val_at)[1]
+
+val_at_target = torch.Tensor(NUM_OF_VAL_AT):fill(CLASS_AT)
+val_lt_target = torch.Tensor(NUM_OF_VAL_LT):fill(CLASS_LT)
+val_at_dataloader = dataload.TensorLoader(val_at, val_at_target)
+val_lt_dataloader = dataload.TensorLoader(val_lt, val_lt_target)
 
 -- 3. define model
 model = nn.Sequential()
@@ -88,12 +95,15 @@ criterion = criterion:cuda()
 -- 6. auto-grad
 parameters,gradParameters = model:getParameters()
 
-function train(dl, batchsize, num_of_batch)
+function train(dl, batchsize, num_of_batch, prefix)
     print('begin trainning')
-    U.dump_para(PREFIX..'-parameters')
-    local time_logger = optim.Logger(PREFIX..'-time.log')
+    model:training()
+    util.dump_para(prefix..'-parameters')
+    local time_logger = optim.Logger(prefix..'-time.log')
     time_logger:setNames{'setup time', 'for-back time', 'batch time'}
-    local train_logger = optim.Logger(PREFIX..'-train.log')
+    local para_logger = optim.Logger(prefix..'-para.log')
+    para_logger:setNames{'atlt split'}
+    local train_logger = optim.Logger(prefix..'-train.log')
     train_logger:setNames{'training error'}
 
     local num_of_batch = num_of_batch or math.floor(NUM_OF_TR_AT / batchsize)
@@ -103,8 +113,8 @@ function train(dl, batchsize, num_of_batch)
 
         -- prepare data
         local timer = torch.Timer()
-        local split = U.get_atlt_split(ATLT_NORM, batchsize)
-        local input, target = U.resample(at_dataloader, lt_dataloader, split, batchsize)
+        local split = util.get_atlt_split(ATLT_NORM, batchsize)
+        local input, target = util.resample(at_dataloader, lt_dataloader, split, batchsize)
         input = input:cuda()
         target = target:cuda()
         local setup_time = timer:time().real
@@ -127,19 +137,44 @@ function train(dl, batchsize, num_of_batch)
 
         local batch_time = batch_timer:time().real
         train_logger:add{criterion.output}
+        para_logger:add{split}
         time_logger:add{setup_time, bf_time, batch_time}
     end
+    model:clearState()
+    torch.save(prefix..'-model.t7',model)
 end
 
 function eval(dl, batchsize)
+    print('begin eval')
+    model:evaluate()
+    local confusion = optim.ConfusionMatrix(NUM_OF_CLASS)
     local at_dataloader, lt_dataloader = table.unpack(dl)
     local function loop(dataloader)
-        local upbound = #dataloader - #dataloader % batchsize
+        local upbound = dataloader:size() - dataloader:size() % batchsize
         for k, inputs, targets in dataloader:subiter(batchsize, upbound) do
             local input = inputs:cuda()
             local target = targets:cuda()
+            confusion:batchAdd(model:forward(input), target)
         end
     end
     loop(at_dataloader)
     loop(lt_dataloader)
+    confusion:updateValids()
+    return confusion
 end
+
+NUM_OF_BAT_IN_1EPOCH = math.floor((NUM_OF_TR_AT * 2) / BAT_SIZE)
+START_ITER = 1
+NUM_OF_ITER = 1
+for i = START_ITER, START_ITER + NUM_OF_ITER - 1 do
+    local prefix = PREFIX .. '-it' .. tostring(i)
+    train({at_dataloader,lt_dataloader}, BAT_SIZE,
+            NUM_OF_BAT_IN_1EPOCH, prefix)
+    local conf = eval({val_at_dataloader,val_lt_dataloader}, BAT_SIZE)
+    print('===it'..i..'===')
+    print(conf)
+end
+
+-- conf = eval({val_at_dataloader,val_lt_dataloader}, BAT_SIZE)
+
+-- print(conf)
